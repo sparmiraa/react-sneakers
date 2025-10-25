dc18c1e3fc11af3619f7bd40e4830ee5edbdf2fb
sparmira <aliceops09@gmail.com>
2025-10-25 20:04:19 +0600

    vercel json added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
- 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT,
-  },
-  plugins: [vercel()],
-});
+  plugins: [react()],
+})
